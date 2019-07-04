5428f6a812821a8fa7110d997d1e41fa0adbffaf
ElHpSoyYo <epalaciol@unal.edu.co>
2019-07-04 18:24:52 -0500

    Propuesta de simulacion

--- Sistema.py
+++ Sistema.py
@@ -31,37 +31,45 @@ class Sistema():
     def relizarSimulacion(self):
         self.personaSubeAscensor(self.ascensor1)
         while(self.tLlegadas):
-            #Condicional V1
-            if (not self.personasEnCola):
-                if (self.ascensor1.estado):
-                    self.personaSubeAscensor(self.ascensor1)
-                elif (self.ascensor2.estado):
-                    self.personaSubeAscensor(self.ascensor2)
-                elif (self.ascensor1.tFinal < self.ascensor2.tfina):
+            """
+            Verificar que accion ocurre primero: 
+                -Llega persona
+                -Llega ascensor 1
+                -Llega ascensor 2
+            Verificar si hay cola:
+                -Hay cola
+                -No hay cola
+            Hacer un producto cartesiano de hay cola o no hay cola:
+                -Llega persona hay cola => forma en cola
+                -Llega ascensor 1 hay cola => termina servicio y sube gente al ascensor
+                -Llega ascensor 2 hay cola => termina servicio y sube gente al ascensor
+                -Llega persona no hay cola => toma ascensor 
+                -Llega ascensor 1 no hay cola => termina servicio 
+                -Llega ascensor 2 no hay cola => termina servicio 
+            """
+            if (self.tLlegadas[-1] < self.ascensor1.tFinal < self.ascensor2.tFinal):
+                if (not self.personasEnCola):
+                    if (self.ascensor1.estado):
+                        self.personaSubeAscensor(self.ascensor1)
+                    elif (self.ascensor2.estado):
+                        self.personaSubeAscensor(self.ascensor2)
+                else:
+                    self.personaEntraALaCola()
+                    
+            elif (self.ascensor1.tFinal < self.ascensor2.tfinal < self.tLlegadas[-1]):
+                if (not self.personasEnCola):
+                    self.ascensor1.terminaServicio()
+                elif (self.personasEnCola):
+                    self.personasEnColaSubenAlAscensor(self.ascensor1)
+
+                """elif (self.ascensor1.tFinal < self.ascensor2.tfina):
                     self.ascensor1.terminaServicio()
                 elif (self.ascensor2.tFinal < self.ascensor1.tfinal):
-                    self.ascensor2.terminaServicio()
-            else:
+                    self.ascensor2.terminaServicio()"""
+            """else:
                 if (self.tLlegadas[-1] < self.ascensor1.tFinal < self.ascensor2.tFinal):
                     self.personaEntraALaCola()
                 elif (self.ascensor1.tFinal < self.ascensor2.tfinal < self.tLlegadas[-1]):
                     self.personasEnColaSubenAlAscensor(self.ascensor1)
                 elif (self.ascensor2.tFinal < self.ascensor1.tfinal < self.tLlegadas[-1]):
-                    self.personasEnColaSubenAlAscensor(self.ascensor2)
-
-            ##Condicional V2
-            if (self.ascensor1.estado and not self.personasEnCola):
-                self.personaSubeAscensor(self.ascensor1)
-            elif (self.ascensor2.estado and not self.personasEnCola):
-                self.personaSubeAscensor(self.ascensor2)
-            elif (self.tLlegadas[-1] < self.ascensor1.tFinal < self.ascensor2.tFinal and self.personasEnCola):
-                self.personaEntraALaCola()
-            elif (self.ascensor1.tFinal < self.ascensor2.tfinal < self.tLlegadas[-1] and self.personasEnCola):
-                self.personasEnColaSubenAlAscensor(self.ascensor1)
-            elif (self.ascensor2.tFinal < self.ascensor1.tfinal < self.tLlegadas[-1] and self.personasEnCola):
-                self.personasEnColaSubenAlAscensor(self.ascensor2)
-            else:
-                if (self.ascensor1.tFinal < self.ascensor2.tfina):
-                    self.ascensor1.terminaServicio()
-                elif (self.ascensor2.tFinal < self.ascensor1.tfinal):
-                    self.ascensor2.terminaServicio()
+                    self.personasEnColaSubenAlAscensor(self.ascensor2)"""
