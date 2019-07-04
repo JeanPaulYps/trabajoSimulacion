a3ebbc9a4f68ace6f50cb9e7f50cea4f86b2503e
ElHpSoyYo <epalaciol@unal.edu.co>
2019-07-04 18:07:32 -0500

    Se arreglaron errores

--- Sistema.py
+++ Sistema.py
@@ -1,4 +1,6 @@
-import Ascensor, Persona 
+from Ascensor import Ascensor
+from Persona import Persona
+
 class Sistema():
     def __init__ (self, ascensor1, ascensor2, tLlegadas):
         self.personasEnCola = []
@@ -8,56 +10,58 @@ class Sistema():
         self.ascensor2 = ascensor2
         self.tLlegadas = tLlegadas
 
-    def personaSubeAscensor(ascensor):
+    def personaSubeAscensor(self, ascensor):
         persona = Persona(self.tLlegadas.pop())
         self.personas.append(persona)
         ascensor.iniciaServicio(persona.tLlegada())
 
-    def personasEnColaSubenAlAscensor(ascensor):
+    def personasEnColaSubenAlAscensor(self, ascensor):
         ascensor.terminaServicio()
         ascensor.iniciaServicio(ascensor.tFinal)
         del(self.personasEnCola[0:8])
 
 
     def personaEntraALaCola(self):
-        persona = Persona(tLlegadas.pop())
+        persona = Persona(self.tLlegadas.pop())
         self.personas.append(persona)
         self.personasEnCola.append(persona)
 
+    
+
     def relizarSimulacion(self):
-        personaSubeAscensor(ascensor1)
+        self.personaSubeAscensor(self.ascensor1)
         while(self.tLlegadas):
             #Condicional V1
             if (not self.personasEnCola):
                 if (self.ascensor1.estado):
-                    personaSubeAscensor(ascensor1)
+                    self.personaSubeAscensor(self.ascensor1)
                 elif (self.ascensor2.estado):
-                    personaSubeAscensor(ascensor2)
-                elif (ascensor1.tFinal < ascensor2.tfina):
-                    ascensor1.terminaServicio()
-                elif (ascensor2.tFinal < ascensor1.tfinal):
-                    ascensor2.terminaServicio()
+                    self.personaSubeAscensor(self.ascensor2)
+                elif (self.ascensor1.tFinal < self.ascensor2.tfina):
+                    self.ascensor1.terminaServicio()
+                elif (self.ascensor2.tFinal < self.ascensor1.tfinal):
+                    self.ascensor2.terminaServicio()
             else:
-                if (tLlegadas[-1] < ascensor1.tFinal < ascensor2.tFinal):
-                    personaEntraALaCola()
-                elif (ascensor1.tFinal < ascensor2.tfinal < tLlegadas[-1]):
-                    personasEnColaSubenAlAscensor(ascensor1)
-                elif (ascensor2.tFinal < ascensor1.tfinal < tLlegadas[-1]):
-                    personasEnColaSubenAlAscensor(ascensor2)
+                if (self.tLlegadas[-1] < self.ascensor1.tFinal < self.ascensor2.tFinal):
+                    self.personaEntraALaCola()
+                elif (self.ascensor1.tFinal < self.ascensor2.tfinal < self.tLlegadas[-1]):
+                    self.personasEnColaSubenAlAscensor(self.ascensor1)
+                elif (self.ascensor2.tFinal < self.ascensor1.tfinal < self.tLlegadas[-1]):
+                    self.personasEnColaSubenAlAscensor(self.ascensor2)
 
             ##Condicional V2
             if (self.ascensor1.estado and not self.personasEnCola):
-                personaSubeAscensor(ascensor1)
+                self.personaSubeAscensor(self.ascensor1)
             elif (self.ascensor2.estado and not self.personasEnCola):
-                personaSubeAscensor(ascensor2)
-            elif (tLlegadas[-1] < ascensor1.tFinal < ascensor2.tFinal and self.personasEnCola):
-                personaEntraALaCola()
-            elif (ascensor1.tFinal < ascensor2.tfinal < tLlegadas[-1] and self.personasEnCola):
-                personasEnColaSubenAlAscensor(ascensor1)
-            elif (ascensor2.tFinal < ascensor1.tfinal < tLlegadas[-1] and self.personasEnCola):
-                personasEnColaSubenAlAscensor(ascensor2)
+                self.personaSubeAscensor(self.ascensor2)
+            elif (self.tLlegadas[-1] < self.ascensor1.tFinal < self.ascensor2.tFinal and self.personasEnCola):
+                self.personaEntraALaCola()
+            elif (self.ascensor1.tFinal < self.ascensor2.tfinal < self.tLlegadas[-1] and self.personasEnCola):
+                self.personasEnColaSubenAlAscensor(self.ascensor1)
+            elif (self.ascensor2.tFinal < self.ascensor1.tfinal < self.tLlegadas[-1] and self.personasEnCola):
+                self.personasEnColaSubenAlAscensor(self.ascensor2)
             else:
-                if (ascensor1.tFinal < ascensor2.tfina):
-                    ascensor1.terminaServicio()
-                elif (ascensor2.tFinal < ascensor1.tfinal):
-                    ascensor2.terminaServicio()
+                if (self.ascensor1.tFinal < self.ascensor2.tfina):
+                    self.ascensor1.terminaServicio()
+                elif (self.ascensor2.tFinal < self.ascensor1.tfinal):
+                    self.ascensor2.terminaServicio()
